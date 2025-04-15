d99771950c15fba1ee492b2f33fe73b4f3957141
Mikael Aditya N <mikaeladityan.99@gmail.com>
2025-04-15 14:36:18 +0700

    Update(Deployment): add ecosytem.config.cjs

--- scripts/fix-imports.sh
+++ scripts/fix-imports.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Temukan semua file .js di folder dist
-find dist -type f -name '*.js' | while read -r file; do
+# find dist -type f -name '*.js' | while read -r file; do
+find dist -type f -name '*.js' -exec sed -i 's/from "\(.*\)";/from "\1.js";/g' {} +
   # Replace semua import tanpa ekstensi ke .js
   sed -i '' -E "s/(from ['\"])(\..*)(['\"])/\1\2.js\3/g" "$file"
 done
